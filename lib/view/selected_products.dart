751c98c286b14c722cf92da7253ad5b630d38f25
Rehan Hamayun <rr8329203@gmail.com>
2022-04-23 16:33:22 +0500

    payment method

--- lib/view/selected_products.dart
+++ lib/view/selected_products.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:get/instance_manager.dart';
+
 import 'package:getx_ecommerce/controller/product_controller.dart';
+import 'package:getx_ecommerce/view/total_amount.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get.dart';
 
 class SelectedProducts extends StatelessWidget {
   ProductController productController = Get.put(ProductController());
@@ -22,40 +23,65 @@ class SelectedProducts extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView.builder(
-          itemCount: productController.cartItem.length,
-          itemBuilder: (context, index) {
-            return Container(
-              height: 200,
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  elevation: 0.0,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                        child: GetX<ProductController>(
-                          builder: (controller) {
-                            return Text(
-                              productController.cartItem[index]['title']
-                                  .toString(),
-                              style: GoogleFonts.poppins(
-                                color: Colors.black,
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: productController.cartItem.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        elevation: 0.0,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: GetX<ProductController>(
+                                builder: (controller) {
+                                  return Text(
+                                    productController.cartItem[index]['title']
+                                        .toString(),
+                                    style: GoogleFonts.poppins(
+                                      color: Colors.black,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  );
+                                },
                               ),
-                            );
-                          },
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 60.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.redAccent,
+              ),
+              onPressed: () {
+                Get.to(() => TotalAmount());
+              },
+              child: Text(
+                "Checkout",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 19,
                 ),
               ),
-            );
-          }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
